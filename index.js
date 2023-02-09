e56c406ae360822c7b046075df6b2405a2ba4f90
rocketman80 <ittipon.rockman80@gmail.com>
2023-02-10 00:21:33 +0700

    fix1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log('API listening on port ${PORT}')
 })
 app.get('/', (req, res) => {
